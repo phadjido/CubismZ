54b65a918d67033801fe7bc8ce93268d637e2538
Panagiotis Hadjidoukas <phadjido@mavt.ethz.ch>
2017-12-04 00:30:36 +0100

    SZ tested

--- CubismApps/Tests/data/test_sz.sh
+++ CubismApps/Tests/data/test_sz.sh
@@ -10,18 +10,14 @@ else
 fi
 
 bs=32
-#ds=512
-ds=1024
+ds=512
 nb=$(echo "$ds/$bs" | bc)
 
 (cd ../tools; make clean; make all sz=1)
 
 rm -f tmp00000.StreamerGridPointIterative.channel0
 
-ulimit -s 1600000000
-export OMP_NUM_THREADS=1
-
 ../tools/hdf2ch -bpdx $nb -bpdy $nb -bpdz $nb -sim io -simdata $h5file  -outdata tmp  -threshold $2
 
-#mpirun -n 8 ../tools/ch2diff -simdata1 tmp00000.StreamerGridPointIterative.channel0  -simdata2 ref.channel0
+mpirun -n 8 ../tools/ch2diff -simdata1 tmp00000.StreamerGridPointIterative.channel0  -simdata2 ref.channel0
 
